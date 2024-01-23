b7d34ed62aecbc2bdb1df64d2c485f4e84f7c1ed
MartinekV <martinekvlastimil95@gmail.com>
2024-01-23 10:31:18 +0100

    Fix api key loading

--- webapp.py
+++ webapp.py
@@ -4,8 +4,14 @@ import os
 
 st.title("Scientific paper QA assistant")
 
+api_input = st.text_input('your OpenAI API key goes here')
+if(api_input):
+    st.write('API KEY UPDATED')
+    os.environ['OPENAI_API_KEY'] = api_input
+    st.write(os.environ['OPENAI_API_KEY'])
+
 #TODO try local llama to avoid api costs
-if "docs" not in st.session_state:
+if "docs" not in st.session_state and api_input:
     docs = Docs()
     st.session_state.docs = docs
 
@@ -23,18 +29,14 @@ for message in st.session_state.messages:
 
 
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
-if uploaded_file is not None:
+if uploaded_file is not None and api_input:
     
     with open(uploaded_file.name, "wb") as f:
         f.write(uploaded_file.read())
         # f.flush()
     st.session_state.docs.add(uploaded_file.name)
 
-api_input = st.text_input('your OpenAI API key goes here')
-if(api_input):
-    st.write('API KEY UPDATED')
-    os.environ['OPENAI_API_KEY'] = api_input
-    st.write(os.environ['OPENAI_API_KEY'])
+
 
 # React to user input
 if prompt := st.chat_input("Ask a question about your files"):
